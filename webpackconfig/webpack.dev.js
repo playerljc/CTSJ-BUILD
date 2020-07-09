eb37a85127b3f5fa6adf46df3a94e2805c7d85b0
zhiqian.liu <zhiqian.liu@baifendian.com>
2020-07-10 06:25:53 +0800

    v1.0.33

--- webpackconfig/webpack.dev.js
+++ webpackconfig/webpack.dev.js
@@ -1,44 +1,48 @@
 // const path = require('path');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const commandArgs = require('../commandArgs');
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const commandArgs = require("../commandArgs");
 const argsMap = commandArgs.initCommandArgs();
 
 // const runtimePath = argsMap.get('--runtimepath')[0];
-const customConfigPath = argsMap.get('--customconfig')[0];
+const customConfigPath = argsMap.get("--customconfig")[0];
 let customModule;
 // if (customConfig !== 'undefined') {
 //   customConfigPath = path.join(runtimePath, customConfig);
 // }
 // else {
-  //   customConfigPath = path.join(runtimePath,'ctbuild.config.js');
+//   customConfigPath = path.join(runtimePath,'ctbuild.config.js');
 // }
 
 // --runtimepath
 // --customconfig
 
 const curModule = merge(common.config, {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
+  mode: "development",
+  devtool: "cheap-module-eval-source-map",
   devServer: {
-    publicPath: '/',
-    host: 'localhost',
+    publicPath: "/",
+    host: "localhost",
     compress: true,
     port: 8000,
-    clientLogLevel: 'none', //不再输出繁琐的信息
+    clientLogLevel: "none", //不再输出繁琐的信息
     historyApiFallback: true,
     overlay: true, //浏览器全屏显示错误信息
+    hot: true, // 启动模块热更新 HMR
+    open: true // 开启自动打开浏览器页面
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process': {
-        'env': {
-          'NODE_ENV': JSON.stringify('development'),
-          'REAP_PATH': JSON.stringify(process.env.REAP_PATH)
+      process: {
+        env: {
+          NODE_ENV: JSON.stringify("development"),
+          REAP_PATH: JSON.stringify(process.env.REAP_PATH)
         }
       }
     }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 });
 
@@ -56,7 +60,7 @@ const curModule = merge(common.config, {
 // const config = merge(curModule, customModule || {});
 // module.exports = config;
 
-const define = argsMap.get('--define')[0] || '';
+const define = argsMap.get("--define")[0] || "";
 
 if (customConfigPath) {
   customModule = require(customConfigPath);
