28c6580243995f75ef840db2c3a6c45100f9e257
zhiqian.liu <zhiqian.liu@baifendian.com>
2020-09-27 15:21:56 +0800

    1.加入了thread-loader 2.加入了alias@ 3.加入了cssModules 4.加入了静态目录assets的赋值 5.加入了evn变量 6.加入了代码分析 7.优化了配置

--- webpackconfig/config/projectCssModules.js
+++ webpackconfig/config/projectCssModules.js
@@ -6,8 +6,8 @@ const path = require('path');
  */
 module.exports = function ({ webpackConfig, plugins, theme = {} }) {
   // include的APP_PATH中的less文件使用cssModules
-  webpackConfig.module.rules[3].use[3].options.modules = true;
-  webpackConfig.module.rules[3].use[5].query.modifyVars = theme;
+  webpackConfig.module.rules[2].use[3].options.modules = true;
+  webpackConfig.module.rules[2].use[5].query.modifyVars = theme;
 
   // include是node_modules中的less文件不需要cssModules
   webpackConfig.module.rules.push({
