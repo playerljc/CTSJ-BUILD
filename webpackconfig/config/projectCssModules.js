b80003e6d4c4bf141b48f6bb1c3d81c1c644b246
zhiqian.liu <zhiqian.liu@baifendian.com>
2020-12-10 16:58:16 +0800

    cssModule修饰符的修改

--- webpackconfig/config/projectCssModules.js
+++ webpackconfig/config/projectCssModules.js
@@ -6,7 +6,9 @@ const { getPostCssConfigPath } = require('../../util');
  */
 module.exports = function ({ webpackConfig, plugins, theme = {}, runtimePath }) {
   // include的APP_PATH中的less文件使用cssModules
-  webpackConfig.module.rules[3].use[3].options.modules = true;
+  webpackConfig.module.rules[3].use[3].options.modules = {
+    localIdentName: '[path][name]__[local]--[hash:base64:5]',
+  };
   webpackConfig.module.rules[3].use[5].query.modifyVars = theme;
 
   // include是node_modules中的less文件不需要cssModules
