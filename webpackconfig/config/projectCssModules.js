17c551e490cbb8789d0da32a86a1cfebd07f78bd
zhiqian.liu <zhiqian.liu@baifendian.com>
2021-01-29 17:12:56 +0800

    [修改] 去掉cache-loader

--- webpackconfig/config/projectCssModules.js
+++ webpackconfig/config/projectCssModules.js
@@ -32,8 +32,8 @@ module.exports = function ({ webpackConfig, plugins, theme = {}, runtimePath }) 
     webpackConfig.module.rules[2].use[3].query.modifyVars = theme;
   } else {
     // include的APP_PATH中的less文件使用cssModules
-    webpackConfig.module.rules[2].use[3].options.modules = true;
-    webpackConfig.module.rules[2].use[5].query.modifyVars = theme;
+    webpackConfig.module.rules[2].use[2].options.modules = true;
+    webpackConfig.module.rules[2].use[4].query.modifyVars = theme;
   }
 
   // include是node_modules中的less文件不需要cssModules
@@ -41,7 +41,7 @@ module.exports = function ({ webpackConfig, plugins, theme = {}, runtimePath }) 
     test: /\.less$/,
     include: [/node_modules/],
     use: [isDev() ? 'style-loader' : plugins.MiniCssExtractPlugin.loader]
-      .concat(isDev() ? [] : ['cache-loader', 'thread-loader'])
+      .concat(isDev() ? [] : ['thread-loader'])
       .concat([
         {
           loader: 'css-loader',
--- webpackconfig/webpack.umdcommon.js
+++ webpackconfig/webpack.umdcommon.js
@@ -80,7 +80,6 @@ module.exports = {
           exclude: /(node_modules|bower_components)/,
           // include: [APP_PATH],
           use: [
-            'cache-loader',
             'thread-loader',
             {
               loader: 'babel-loader',
@@ -111,7 +110,6 @@ module.exports = {
           include: [APP_PATH, /highlight.js/, /photoswipe.css/, /default-skin.css/],
           use: [
             isDev() ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'cache-loader',
             'thread-loader',
             {
               loader: 'css-loader',
@@ -134,7 +132,6 @@ module.exports = {
           include: [APP_PATH, /normalize.less/],
           use: [
             isDev() ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'cache-loader',
             'thread-loader',
             {
               loader: 'css-loader',
