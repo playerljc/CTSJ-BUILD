bf8819fccbf0b2bb9ffb50cecd2a40abe514106b
zhiqian.liu <1qaz@WSX>
2022-01-13 14:05:21 +0800

    [update] gulp的copy修改

--- gulpfile.js
+++ gulpfile.js
@@ -30,9 +30,11 @@ const compilePath = argsMap.get('--compilepath')[0];
  * copy
  */
 gulp.task('copy', () => {
-  for (let i = 0; i < copyexts.length; i++) {
-    gulp.src(path.join(compilePath, '**', `*.${copyexts[i]}`)).pipe(gulp.dest(outputpath));
-  }
+  // for (let i = 0; i < copyexts.length; i++) {
+  //   gulp.src(path.join(compilePath, '**', `*.${copyexts[i]}`)).pipe(gulp.dest(outputpath));
+  // }
+  const srcs = copyexts.map(ext => path.join(compilePath, '**', `*.${ext}`));
+  return gulp.src(srcs).pipe(gulp.dest(outputpath));
 });
 
 /**
